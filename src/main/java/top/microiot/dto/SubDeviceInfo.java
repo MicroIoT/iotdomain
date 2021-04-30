9d55dc83ea6ddbe4bdaf6bf636d3207bfaffa5fd
caoxinyu <caoxinyu@gmail.com>
2021-04-30 09:37:35 +0800

    gateway id check

--- src/main/java/top/microiot/dto/SubDeviceInfo.java
+++ src/main/java/top/microiot/dto/SubDeviceInfo.java
@@ -1,6 +1,9 @@
 package top.microiot.dto;
 
+import javax.validation.constraints.NotEmpty;
+
 public class SubDeviceInfo<T> extends DeviceInfo<T> {
+	@NotEmpty(message="gatewayId can't be empty")
 	private String gatewayId;
 
 	public String getGatewayId() {
